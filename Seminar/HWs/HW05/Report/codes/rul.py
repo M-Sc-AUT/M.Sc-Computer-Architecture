b674a566f455e7cbe74cedbf9fdb29e0253671ac
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-08-01 17:02:47 +0330

    edit report

--- Seminar/HWs/HW05/Report/codes/rul.py
+++ Seminar/HWs/HW05/Report/codes/rul.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 
 t = np.arange(0, 120, 1)
 
+f = 1 - (t / 120)
 
-
-plt.plot(t, -t, linewidth=2, color='blue')
+plt.plot(t, f, linewidth=2, color='blue', linestyle='-.')
 plt.xlabel("Number of Samples (time)")
 plt.ylabel("RUL")
+# plt.ylim(0, 1)
 plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
 
 
