f3539bbd719b88372e3099d99658fea109f7d6ad
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-12-11 23:53:43 +0330

    add while true main.py

--- Dependable System Design/Project/Project1/Codes/main.py
+++ Dependable System Design/Project/Project1/Codes/main.py
@@ -1,10 +1,16 @@
 from utils import *
 
 
-# Example input from user
-system_input = input("Enter the system configuration: ")
 
-# Calculate total reliability
-total_reliability = calculate_total_reliability(system_input)
+while True:
+    # Example input from user
+    system_input = input("Enter the system configuration: ")
 
-print(f"Total Reliability of the System: {total_reliability:.4f}")
+    if(system_input == "-1"):
+        print("Exiting...")
+        break
+    else:
+        # Calculate total reliability
+        total_reliability = calculate_total_reliability(system_input)
+
+        print(f"Total Reliability of the System: {total_reliability:.4f}")
