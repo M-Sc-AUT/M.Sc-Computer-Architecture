d0ca3e7885819aa006e40bee83615e2ce4466475
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-12-15 10:41:53 +0330

    add reliability plot

---
+++ Dependable System Design/HWs/HW08/hot_cold_reliability.m
@@ -0,0 +1,21 @@
+clear; clc; close all;
+
+
+%%%%%%%%%%%%% constant values %%%%%%%%%%%%%
+landa = 0.4;
+t = 0:0.001:10;
+
+%%%%%%%%%%%%% reliability functions %%%%%%%%%%%%%
+R_cold = exp(-landa .* t) + landa .* t .* exp(-landa .* t);
+R_hot = exp(-landa .* t) + exp(-landa .* t) - exp(-(landa + landa) .* t);
+
+%%%%%%%%%%%%% plots %%%%%%%%%%%%%
+figure(1);
+plot(t, R_cold, LineWidth=1.2, Color="blue");
+hold on;
+plot(t, R_hot, LineWidth=1.2, Color="red");
+legend('R_{cold}(t)','R_{hot}(t)');
+title("Cold Stand By Spare Reliability VS. Hot Stand By Spare (\lambda=0.4)");
+xlabel("time");
+ylabel("R(t)");
+grid on;
